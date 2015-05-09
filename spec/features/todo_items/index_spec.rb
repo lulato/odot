829875437c5a60ab69cb8777fcbf08e559d05108
luis la torre <luisrlatorre@gmail.com>
2015-05-09 13:22:43 -0400

    write test for todo items

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -2,11 +2,39 @@ require "spec_helper"
 
 describe "viewing todo items" do
   let!(:todo_list) {TodoList.create(title: "Grocery List", description: "Groceries")}
-  it "displays no items when a todo list is empty" do
+
+  def visit_todo_list(list)
     visit "/todo_lists"
-    within "#todo_list_#{todo_list.id}" do
+    within "#todo_list_#{list.id}" do
       click_link "List Items"
     end
-    expect(page).to have_content("TodoItems#index")
   end
+
+  it "displays the title of the to do list" do
+    visit_todo_list(todo_list)
+    within("h1") do
+      expect(page).to have_content(todo_list.title)
+    end
+  end
+
+  it "displays no items when a todo list is empty" do
+    visit_todo_list(todo_list)
+    expect(page.all("ul.todo_items li").size).to eq(0)
+  end
+
+
+  it "displays items when a todo list has items" do
+    todo_list.todo_items.create(content: "Buy Milk")
+    todo_list.todo_items.create(content: "Walk the Dog")
+
+    visit_todo_list(todo_list)
+    expect(page.all("ul.todo_items li").size).to eq(2)
+
+    within "ul.todo_items" do 
+      expect(page).to have_content("Buy Milk")
+      expect(page).to have_content("Walk the Dog")
+    end
+
+  end
+
 end
